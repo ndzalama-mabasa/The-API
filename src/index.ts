157d2f7c77946b11c946fb04f45e3f044cc37571
Ndzalama Mabasa <Ndzalama.Mabasa@bbd.co.za>
2025-01-21 00:01:35 +0200

    add random code

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 export const handler = async (event) => {
+  const randomNumber = Math.random();
   // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
+    body: JSON.stringify(`Hello from Lambda with code ${randomNumber}`),
   };
   return response;
 };
