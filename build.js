a5d3217a3ef1f6c9df3a23922ec453333a155562
Ndzalama Mabasa <Ndzalama.Mabasa@bbd.co.za>
2025-01-21 08:13:22 +0200

    fix format

--- build.js
+++ build.js
@@ -6,8 +6,7 @@ esbuild
     entryPoints: ["./src/index.ts"], // Your entry file
     bundle: false, // Disable bundling if you want separate files
     outdir: "./", // Output directory
-    format: "cjs", // Output as ES modules
+    format: "esm",
     target: "ES2020", // Target environment
-    module: "ESNext",
   })
   .catch(() => process.exit(1));
